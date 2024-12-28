432b33b528e20b21066bc39dfb36f10a6bd8f8fd
Avnish Jain <avnish.jain@solugenix.com>
2024-12-28 22:02:31 +0530

    Final Dashboard Code connected with backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
@@ -58,6 +58,7 @@ const uxModules = [
     BrowserAnimationsModule,
     AppRoutingModule, 
     HighchartsChartModule,
+    HttpClientModule,
     uxModules
   ],
   providers: [],
