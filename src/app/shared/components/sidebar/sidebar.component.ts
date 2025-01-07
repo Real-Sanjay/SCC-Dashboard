9e94e49dc19159cbd35216b8ed7ec29aa88a4ca0
Niharika <20311a04f9@sreenidhi.edu.in>
2025-01-07 11:22:26 +0530

    updated v2.1

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -9,22 +9,9 @@ import { PopUpService } from 'src/app/core/services/pop-up-service';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-  // email: string = "";
-  // role:string = "";
+
 
   constructor(private authService: AuthService, private router: Router,private popUpService: PopUpService) {}
-
-  // ngOnInit(): void {
-  //   this.authService.getUserDetails().subscribe({
-  //     next: (data) => {
-  //       this.email = data.email;
-  //       this.role = data.role;
-  //     },
-  //     error: (err) => {
-  //       console.error('Failed to fetch user details:', err);
-  //     },
-  //   });
-  // }
 
   logout(): void {
     this.popUpService.confirm('Are you sure you want to Logout?').subscribe(result=>{
